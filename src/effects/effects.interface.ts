93f52b5ef92d76cb544bdbe9eb9ea1eaaa36e5b1
Jozef Flakus <monodies@me.com>
2018-05-05 00:02:22 +0200

    improvement - EffectResponse as interface

--- src/effects/effects.interface.ts
+++ src/effects/effects.interface.ts
@@ -2,11 +2,11 @@ import { Observable } from 'rxjs';
 import { HttpRequest, HttpResponse } from '../http.interface';
 import { StatusCode } from '../util';
 
-export type EffectResponse = {
-  status: StatusCode,
-  body?: object,
-  headers?: Record<string, string>,
-};
+export interface EffectResponse {
+  status: StatusCode;
+  body?: object;
+  headers?: Record<string, string>;
+}
 
 export type Effect<T = EffectResponse, U = any> = (
   request$: Observable<HttpRequest>,
