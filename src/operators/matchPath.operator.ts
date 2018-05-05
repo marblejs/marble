6edc799d16c9576bb4e7fa281f54d0f31d45a970
Jozef Flakus <monodies@me.com>
2018-05-05 23:50:32 +0200

    improvement - matchPath operator - added regexpt pattern matching

--- src/operators/matchPath.operator.ts
+++ src/operators/matchPath.operator.ts
@@ -1,8 +1,9 @@
+import * as pathToRegexp from 'path-to-regexp';
 import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { HttpRequest } from '../http.interface';
 
 export const matchPath = (path: string) => (source$: Observable<HttpRequest>) =>
   source$.pipe(
-    filter(req => req.url === path)
+    filter(req => pathToRegexp(path).test(req.url!))
   );
