66bd46021ac5c52f27c3000f074054de434d3b9d
Jozef Flakus <monodies@me.com>
2018-05-24 22:48:45 +0200

    test(core): matcher.factory test cases

--- packages/core/src/operators/matchPath/matchPath.operator.ts
+++ packages/core/src/operators/matchPath/matchPath.operator.ts
@@ -2,7 +2,7 @@ import * as pathToRegexp from 'path-to-regexp';
 import { Observable } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
 import { HttpRequest } from '../../http.interface';
-import { pathFactory, urlFactory } from './matchPath.factory';
+import { matcherFactory, removeQueryParams } from './matcher.factory';
 import { queryParamsFactory } from './queryParams.factory';
 import { urlParamsFactory } from './urlParams.factory';
 
@@ -19,9 +19,9 @@ export const matchPath = (path: string, opts: MatcherOpts = {}) => (
     tap(req => (req.params = req.params || {})),
     tap(req => (req.matchers = req.matchers || [])),
     filter(req => {
-      const match = pathFactory(req.matchers!, path, opts.suffix);
-      const url = urlFactory(req.url!);
-      return pathToRegexp(match).test(url);
+      const matcher = matcherFactory(req.matchers!, path, opts.suffix);
+      const url = removeQueryParams(req.url!);
+      return pathToRegexp(matcher).test(url);
     }),
     tap(req => (req.params = urlParamsFactory(req, path))),
     tap(req => (req.query = queryParamsFactory(req.url!))),
