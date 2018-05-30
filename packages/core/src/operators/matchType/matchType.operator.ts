839e0215f88859ce8cabbf21d02ca08db0773a68
Jozef Flakus <monodies@me.com>
2018-05-30 23:53:47 +0200

    feat(core): matchType - match all types using * sign

--- packages/core/src/operators/matchType/matchType.operator.ts
+++ packages/core/src/operators/matchType/matchType.operator.ts
@@ -2,8 +2,8 @@ import { Observable } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
 import { HttpMethod, HttpRequest } from '../../http.interface';
 
-export const matchType = (method: HttpMethod) => (source$: Observable<HttpRequest>) =>
+export const matchType = (method: HttpMethod | '*') => (source$: Observable<HttpRequest>) =>
   source$.pipe(
-    filter(req => req.method === method),
+    filter(req => req.method === method || method === '*'),
     tap(req => req.matchType = true),
   );
