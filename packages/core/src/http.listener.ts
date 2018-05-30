df2c85b6d4fe07a64860c32604966f5d145b75e2
Jozef Flakus <monodies@me.com>
2018-05-30 19:43:08 +0200

    build(prettier): removed prettier

--- packages/core/src/http.listener.ts
+++ packages/core/src/http.listener.ts
@@ -1,20 +1,7 @@
 import { Subject, of } from 'rxjs';
-import {
-  catchError,
-  defaultIfEmpty,
-  mergeMap,
-  switchMap,
-  tap,
-} from 'rxjs/operators';
-import {
-  combineEffects,
-  combineMiddlewareEffects,
-} from './effects/effects.combiner';
-import {
-  Effect,
-  EffectResponse,
-  GroupedEffects,
-} from './effects/effects.interface';
+import { catchError, defaultIfEmpty, mergeMap, switchMap, tap } from 'rxjs/operators';
+import { combineEffects, combineMiddlewareEffects } from './effects/effects.combiner';
+import { Effect, EffectResponse, GroupedEffects } from './effects/effects.interface';
 import { Http, HttpRequest, HttpResponse, HttpStatus } from './http.interface';
 import { getErrorMiddleware } from './middlewares/error.middleware';
 import { handleResponse } from './response/response.handler';
