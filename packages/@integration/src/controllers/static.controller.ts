6e48f292611d2774adef80a8fd3686e22905c7e3
JozefFlakus <monodies@me.com>
2018-09-07 13:43:29 +0200

    feat(integration): removed `@example` package and combined it with `@integration`

---
+++ packages/@integration/src/controllers/static.controller.ts
@@ -0,0 +1,21 @@
+import * as path from 'path';
+import { EffectFactory, combineRoutes, internal } from '@marblejs/core';
+import { map, switchMap } from 'rxjs/operators';
+const { readFile } = internal;
+
+const STATIC_PATH = path.resolve(__dirname, '../../../../assets');
+
+const getFile$ = EffectFactory
+  .matchPath('/:dir')
+  .matchType('GET')
+  .use(req$ => req$
+    .pipe(
+      map(req => req.params!.dir as string),
+      switchMap(readFile(STATIC_PATH)),
+      map(body => ({ body }))
+    ));
+
+export const static$ = combineRoutes(
+  '/static',
+  [ getFile$ ],
+);
