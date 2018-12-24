5e626b9ecce5bd5814f7ef5433944c4367b8f889
Jozef Flakus <monodies@me.com>
2018-12-24 14:05:35 +0100

    imp(@integration): don't show logger output during tests

--- packages/@integration/src/middlewares/logger.middleware.ts
+++ packages/@integration/src/middlewares/logger.middleware.ts
@@ -2,9 +2,10 @@ import { loggerWithOpts$ } from '@marblejs/middleware-logger';
 import { createWriteStream } from 'fs';
 import { join } from 'path';
 
+const silent = process.env.NODE_ENV === 'test';
 const writePath = join(__dirname, '../../', 'access.log');
 const stream = createWriteStream(writePath, { flags: 'a' });
-const options = { silent: false, stream };
+const options = { silent, stream };
 
 export const loggerDev$ = loggerWithOpts$();
 export const loggerFile$ = loggerWithOpts$(options);
