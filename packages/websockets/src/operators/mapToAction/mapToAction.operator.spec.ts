1f6f495ef9c0269b15594b3805275ddc3c9c5326
Jozef Flakus <monodies@me.com>
2018-12-26 23:08:20 +0100

    test(websockets): mapToAction operator test cases

--- packages/websockets/src/operators/mapToAction/mapToAction.operator.spec.ts
+++ packages/websockets/src/operators/mapToAction/mapToAction.operator.spec.ts
@@ -1,5 +1,26 @@
-describe('mapToAction.operator', () => {
-  test('@TODO', () => {
-    // @TODO
+import { Marbles } from '@marblejs/core/dist/+internal';
+import { mapToAction } from './mapToAction.operator';
+import { WebSocketEffect } from '../../effects/ws-effects.interface';
+
+describe('#mapToAction operator', () => {
+  test('factorizes WebSocket response', () => {
+    // given
+    const incomingEvent = { type: 'TEST_EVENT' };
+    const outgoingEvent = { type: 'TEST_EVENT', payload: 'test_payload' };
+
+    // when
+    const effect$: WebSocketEffect = event$ =>
+      event$.pipe(
+        mapToAction((event, creator) => creator
+          .type(event.type)
+          .payload('test_payload'),
+        ),
+      );
+
+    // then
+    Marbles.assertEffect(effect$, [
+      ['--a--', { a: incomingEvent }],
+      ['--b--', { b: outgoingEvent }],
+    ]);
   });
 });
