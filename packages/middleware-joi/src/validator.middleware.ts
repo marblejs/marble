5a2e3a3445f871d060b2ad672e4075e420a18793
Józef Flakus <monodies@me.com>
2019-01-31 18:35:33 +0100

    imp(middlware-joi): package deprecation (#99)

--- packages/middleware-joi/src/validator.middleware.ts
+++ packages/middleware-joi/src/validator.middleware.ts
@@ -23,9 +23,16 @@ const validateSource = (rules: Map<string, any>, options: Joi.ValidationOptions)
     mapTo(req)
   );
 
+/**
+ * @deprecated [#1] since version 2.0,
+ * [#2] use @marblejs/middlware-io instead,
+ */
 export const validator$ = <TBody = any, TParams = any, TQuery = any>
   (schema: Partial<Schema<TBody, TParams, TQuery>>, options: Joi.ValidationOptions = {}) =>
   (req$: Observable<HttpRequest>) => {
+    // tslint:disable-next-line:max-line-length
+    console.warn('Deprecation warning: @marblejs/middleware-joi is deprecated since v2.0. Use @marblejs/middlware-io instead.');
+
     const result = Joi.validate(schema, SchemaValidator);
     const rules = Object.keys(schema).reduce(
       (acc, value) => acc.set(value, Joi.compile(schema[value])),
