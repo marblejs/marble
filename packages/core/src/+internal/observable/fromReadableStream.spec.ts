afc8632ce502d5a27cedb6c7420f79a3f00770b7
JozefFlakus <monodies@me.com>
2019-02-05 16:07:26 +0100

    test(middleware-body): added missing test cases

---
+++ packages/core/src/+internal/observable/fromReadableStream.spec.ts
@@ -0,0 +1,51 @@
+import { fromReadableStream } from './fromReadableStream';
+import { Transform } from 'stream';
+
+class DuplexStream extends Transform {
+  _transform(chunk, _, callback) {
+    this.push(chunk);
+    callback();
+  }
+}
+
+describe('#fromReadableStream', () => {
+  test('reads data to buffer', done => {
+    const stream = new DuplexStream();
+    const data = '111';
+
+    fromReadableStream(stream).subscribe(
+      (buffer: Buffer) => {
+        expect(buffer.toString()).toEqual(data);
+      },
+      (error: Error) => {
+        fail(`Error shouldn't be thrown: ${error}`);
+        done();
+      },
+      () => {
+        done();
+      },
+    );
+
+    stream.write(data);
+    stream.end();
+  });
+
+  test('handles stream error', done => {
+    const stream = new DuplexStream();
+    const errorToThrow = new Error('test-error');
+
+    fromReadableStream(stream).subscribe(
+      (buffer: Buffer) => {
+        fail(`Error should be thrown`);
+        done();
+      },
+      (error: Error) => {
+        expect(error.message).toBe(errorToThrow.message);
+        done();
+      }
+    );
+
+    stream.emit('error', errorToThrow);
+    stream.end();
+  });
+});
