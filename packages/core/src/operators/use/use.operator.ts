a34edca59faec94ddd9b0edff9f124d89a3ba22c
Jozef Flakus <monodies@me.com>
2019-02-24 20:38:36 +0100

    imp(core): updated type declaration for use operator

--- packages/core/src/operators/use/use.operator.ts
+++ packages/core/src/operators/use/use.operator.ts
@@ -1,12 +1,13 @@
 import { Observable, of } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
+import { EffectMetadata } from '../../effects/effects.interface';
 
 interface MiddlewareLike<I, O> {
   (i$: Observable<I>, client: any, meta: any): Observable<O>;
 }
 
 export const use = <I, O>
-  (middleware: MiddlewareLike<I, O>, client?: any, meta?: any) =>
+  (middleware: MiddlewareLike<I, O>, client?: any, meta?: EffectMetadata) =>
   (source$: Observable<I>) =>
     source$.pipe(
       mergeMap(req => middleware(of(req), client!, meta!) as Observable<O>)
