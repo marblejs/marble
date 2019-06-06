26d928309b5bdc3d93896bb054514e648084353b
Józef Flakus <monodies@me.com>
2019-06-06 08:45:28 +0200

    Update responseBody.factory.ts

--- packages/core/src/response/responseBody.factory.ts
+++ packages/core/src/response/responseBody.factory.ts
@@ -2,9 +2,10 @@ import { HttpHeaders } from '../http.interface';
 import { ContentType, isStream } from '../+internal';
 
 export const bodyFactory = (headers: HttpHeaders) => (body: any) => {
-  if(isStream(body)){
+  if (isStream(body)) {
     return body;
   }
+  
   switch (headers['Content-Type']) {
     case ContentType.APPLICATION_JSON:
       return JSON.stringify(body);
