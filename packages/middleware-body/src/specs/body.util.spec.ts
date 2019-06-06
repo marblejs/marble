150839eff71a4599dd82d7620af64bfb5d6a433d
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2019-06-06 20:17:57 +0200

    imp(http helper): simplified `createHttpRequest` function and it's usages

--- packages/middleware-body/src/specs/body.util.spec.ts
+++ packages/middleware-body/src/specs/body.util.spec.ts
@@ -2,9 +2,9 @@ import { createHttpRequest } from '@marblejs/core/dist/+internal/testing/http.he
 import { hasBody } from '../body.util';
 
 test('#hasBody checks if request has body', () => {
-  expect(hasBody(createHttpRequest({ url: '/', body: null }))).toEqual(false);
-  expect(hasBody(createHttpRequest({ url: '/', body: undefined }))).toEqual(false);
-  expect(hasBody(createHttpRequest({ url: '/', body: 'test' }))).toEqual(true);
-  expect(hasBody(createHttpRequest({ url: '/', body: {} }))).toEqual(true);
-  expect(hasBody(createHttpRequest({ url: '/', body: 1 }))).toEqual(true);
+  expect(hasBody(createHttpRequest({ body: null }))).toEqual(false);
+  expect(hasBody(createHttpRequest({ body: undefined }))).toEqual(false);
+  expect(hasBody(createHttpRequest({ body: 'test' }))).toEqual(true);
+  expect(hasBody(createHttpRequest({ body: {} }))).toEqual(true);
+  expect(hasBody(createHttpRequest({ body: 1 }))).toEqual(true);
 });
