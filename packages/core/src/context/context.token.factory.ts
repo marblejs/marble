e793617276f7151d1148c34ee86d73b950ae96e8
Jozef Flakus <monodies@me.com>
2019-08-05 16:40:16 +0200

    feat(core): pass name to `createContextToken` factory

--- packages/core/src/context/context.token.factory.ts
+++ packages/core/src/context/context.token.factory.ts
@@ -3,7 +3,10 @@ import * as uuid from 'uuid';
 export class ContextToken<T = any> {
   _id = uuid();
   _T!: T;
+  constructor(public name?: string) {}
 }
 
-export const createContextToken = <T>() =>
-  new class extends ContextToken<T> {};
+export const createContextToken = <T>(name?: string) =>
+  new class extends ContextToken<T> {
+    constructor() { super(name); }
+  };
