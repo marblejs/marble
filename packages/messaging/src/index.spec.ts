cef9214d790a84d75a0942f50b0f12506059f596
Jozef Flakus <monodies@me.com>
2019-10-13 23:16:27 +0200

    buil(all): improvement - publish:canary:major:github

--- packages/messaging/src/index.spec.ts
+++ packages/messaging/src/index.spec.ts
@@ -4,6 +4,7 @@ describe('@marblejs/messaging', () => {
   test('public APIs are defined', () => {
     expect(API.Transport).toBeDefined();
     expect(API.TransportLayerToken).toBeDefined();
+    expect(API.ServerEvent).toBeDefined();
     expect(API.createMicroservice).toBeDefined();
     expect(API.messagingClient).toBeDefined();
     expect(API.messagingListener).toBeDefined();
