56bd886653640c2580e0ba60a5bebc5885c42b81
JozefFlakus <monodies@me.com>
2020-01-28 16:44:41 +0100

    feat(core): act operator

---
+++ packages/core/src/operators/act/act.operator.ts
@@ -0,0 +1,50 @@
+import * as O from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/pipeable';
+import { Observable, of, defer, isObservable } from 'rxjs';
+import { mergeMap, catchError } from 'rxjs/operators';
+import { Event } from '../../event/event.interface';
+
+export function act<
+  InputEvent extends Event,
+  CallEvent extends Event,
+>(
+  callFn: (event: InputEvent) => Observable<CallEvent>,
+): (source: Observable<InputEvent>) => Observable<CallEvent>;
+
+export function act<
+  InputEvent extends Event,
+  CallEvent extends Event,
+  ErrorEvent extends Event,
+>(
+  callFn: (event: InputEvent) => Observable<CallEvent>,
+  errorFn: (error: any, event: InputEvent) => ErrorEvent | Observable<ErrorEvent>,
+): (source: Observable<InputEvent>) => Observable<CallEvent | ErrorEvent>;
+
+export function act<
+  InputEvent extends Event,
+  CallEvent extends Event,
+  ErrorEvent extends Event,
+>(
+  callFn: (event: InputEvent) => Observable<CallEvent>,
+  errorFn?: (error: any, event: InputEvent) => ErrorEvent | Observable<ErrorEvent>,
+) {
+
+  const getDefaultErrorEvent = (error: Error) => (event: Event) => of({
+    type: event.type,
+    error: { name: error.name, message: error.message },
+  } as ErrorEvent);
+
+  return (source: Observable<InputEvent>): Observable<CallEvent | ErrorEvent> =>
+    source.pipe(
+      mergeMap(event => defer(() =>
+        callFn(event).pipe(
+          catchError(error => pipe(
+            O.fromNullable(errorFn),
+            O.map(fn => fn(error, event)),
+            O.map(res => !isObservable(res) ? of(res) : res),
+            O.getOrElse(() => getDefaultErrorEvent(error)(event)),
+          )),
+        ),
+      )),
+    );
+}
--- packages/core/src/operators/index.ts
+++ packages/core/src/operators/index.ts
@@ -1,3 +1,4 @@
 export { use } from './use/use.operator';
+export { act } from './act/act.operator';
 export { switchToProtocol } from './switchToProtocol/switchToProtocol.operator';
 export { matchEvent } from './matchEvent/matchEvent.operator';
