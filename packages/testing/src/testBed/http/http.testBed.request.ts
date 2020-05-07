ae8597365bad455b5b99796f5f7e89e9051faa6a
Jozef Flakus <monodies@me.com>
2020-05-07 23:51:18 +0200

    fix(testing): auto detect `Content-Type` when passing HTTP request body

--- packages/testing/src/testBed/http/http.testBed.request.ts
+++ packages/testing/src/testBed/http/http.testBed.request.ts
@@ -2,6 +2,7 @@ import { HttpMethod, HttpHeaders } from '@marblejs/core'
 import * as O from 'fp-ts/lib/Option';
 import * as A from 'fp-ts/lib/Array';
 import { pipe } from 'fp-ts/lib/pipeable';
+import { getMimeType } from '@marblejs/core/dist/http/response/http.responseContentType.factory';
 import { HttpTestBedRequest, WithBodyApplied } from './http.testBed.request.interface';
 import { createTestingRequestHeader } from './http.testBed.util';
 
@@ -28,10 +29,16 @@ export const withHeaders = (headers: HttpHeaders) => <T extends HttpMethod>(req:
   headers: { ...req.headers, ...headers },
 });
 
-export const withBody = <T>(body: T) => <U extends 'POST' | 'PUT' | 'PATCH'>(req: HttpTestBedRequest<U>): HttpTestBedRequest<U> & WithBodyApplied<T> => ({
-  ...req,
-  body,
-});
+export const withBody = <T>(body: T) => <U extends 'POST' | 'PUT' | 'PATCH'>(req: HttpTestBedRequest<U>): HttpTestBedRequest<U> & WithBodyApplied<T> =>
+  pipe(
+    getHeader('Content-Type')(req),
+    O.map(() => ({ ...req, body })),
+    O.getOrElse(() => pipe(
+      getMimeType(body, req.path),
+      type => withHeaders({ 'Content-Type': type })(req),
+      req => ({ ...req, body }),
+    ))
+  );
 
 export const getHeader = <T extends string = string>(key: string) => (req: HttpTestBedRequest<any>): O.Option<T> =>
   pipe(
