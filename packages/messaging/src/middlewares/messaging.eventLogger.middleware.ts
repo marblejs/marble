ed5c984053c06c0e772abaeabce42e06defa5fbf
Jozef Flakus <monodies@me.com>
2020-08-25 20:57:48 +0200

    feat(messaging): detailed logs for event validation errors

--- packages/messaging/src/middlewares/messaging.eventLogger.middleware.ts
+++ packages/messaging/src/middlewares/messaging.eventLogger.middleware.ts
@@ -1,70 +1,108 @@
+import { pipe } from 'fp-ts/lib/pipeable';
 import { tap } from 'rxjs/operators';
-import { useContext, LoggerToken, LoggerLevel } from '@marblejs/core';
-import { isNamedError } from '@marblejs/core/dist/+internal/utils';
+import { useContext, LoggerToken, LoggerLevel, Event, EventError, isEventError } from '@marblejs/core';
+import { NamedError } from '@marblejs/core/dist/+internal/utils';
 import { MsgMiddlewareEffect, MsgOutputEffect } from '../effects/messaging.effects.interface';
 import { TransportLayerToken } from '../server/messaging.server.tokens';
 
+/**
+ * Logs incoming events
+ *
+ * @since v3.0.0
+ * @param event$ - event stream
+ * @param ctx - EffectContext
+ */
 export const inputLogger$: MsgMiddlewareEffect = (event$, ctx) => {
-  const logger = useContext(LoggerToken)(ctx.ask);
-  const transportLayer = useContext(TransportLayerToken)(ctx.ask);
-
-  return event$.pipe(
-    tap(event => {
-      const tag = transportLayer.config.channel;
-      const id = event.metadata?.correlationId;
-      const message = id ? `${event.type}, id: ${id}` : event.type;
-
-      logger({
-        tag,
-        message,
-        type: 'EVENT_IN',
-        level: LoggerLevel.INFO,
-      })();
-    }),
-  );
-};
-
-export const outputLogger$: MsgOutputEffect = (event$, ctx) => {
-  const logger = useContext(LoggerToken)(ctx.ask);
-  const transportLayer = useContext(TransportLayerToken)(ctx.ask);
-
-  return event$.pipe(
-    tap(event => {
-      const { error, metadata, type } = event;
-      const tag = transportLayer.config.channel;
-      const message = error
-        ? `${isNamedError(error) ? `"${error.name}", "${error.message}"` : 'error'} for event ${type}`
-        : metadata?.replyTo
-          ? `${event.type}, id: ${metadata?.correlationId || '-'} and sent to "${metadata.replyTo || '-'}"`
-          : metadata?.correlationId
-            ? `${event.type}, id: ${metadata.correlationId}`
-            : event.type
-
-      logger({
-        tag,
-        message,
-        level: event.error ? LoggerLevel.ERROR : LoggerLevel.INFO,
-        type: 'EVENT_OUT',
-      })();
-    }),
-  );
-};
-
-export const errorLogger$: MsgMiddlewareEffect = (event$, ctx) => {
   const logger = useContext(LoggerToken)(ctx.ask);
   const transportLayer = useContext(TransportLayerToken)(ctx.ask);
   const tag = transportLayer.config.channel;
 
   return event$.pipe(
-    tap(event => {
-      const message = `"${event.error?.name || '-'}: ${event.error?.message || '-' }" for event ${event.type}`;
-
-      return logger({
-        tag,
-        message,
-        type: 'ERROR',
-        level: LoggerLevel.ERROR,
-      })();
-    }),
+    tap(event => pipe(
+      getIncomingMessageForEvent(event),
+      message => logger({ tag, message, level: LoggerLevel.INFO, type: 'EVENT_IN' }),
+    )()),
   );
 };
+
+/**
+ * Logs outgoing events
+ *
+ * @since v3.0.0
+ * @param event$ - event stream
+ * @param ctx - EffectContext
+ */
+export const outputLogger$: MsgOutputEffect = (event$, ctx) => {
+  const logger = useContext(LoggerToken)(ctx.ask);
+  const transportLayer = useContext(TransportLayerToken)(ctx.ask);
+  const tag = transportLayer.config.channel;
+
+  return event$.pipe(
+    tap(event => pipe(
+      event.error
+        ? getErrorMessageForEvent(event)
+        : getOutgoingMessageForEvent(event),
+      message => logger({ tag, message, level: event.error ? LoggerLevel.ERROR : LoggerLevel.INFO, type: 'EVENT_OUT' })
+    )()),
+  );
+};
+
+/**
+ * Logs server exceptions
+ *
+ * @since v3.0.0
+ * @param event$ - event stream
+ * @param ctx - EffectContext
+ */
+export const exceptionLogger$: MsgMiddlewareEffect = (event$, ctx) => {
+  const logger = useContext(LoggerToken)(ctx.ask);
+  const transportLayer = useContext(TransportLayerToken)(ctx.ask);
+  const tag = transportLayer.config.channel;
+
+  return event$.pipe(
+    tap(event => pipe(
+      getErrorMessageForEvent(event),
+      message => logger({ tag, message, level: LoggerLevel.ERROR, type: 'ERROR' }),
+    )()),
+  );
+};
+
+const getValidationErrorMessage = (event: Event<unknown, EventError>): string => {
+  const error = event.error;
+  const correlationId = event.metadata?.correlationId || 'UNKNOWN_CORRELATION_ID';
+  const parsedError = error ? JSON.stringify(error.data) : 'UNKNOWN_ERROR';
+  const eventType = event.type.replace('_UNHANDLED_ERROR', '');
+  return correlationId
+    ? `Received invalid event "${eventType}" (${correlationId}) with error: ${parsedError}`
+    : `Received invalid event "${eventType}" with error: ${parsedError}`;
+}
+
+const getGenericErrorMessage = (event: Event<unknown, NamedError>): string => {
+  const error = event.error;
+  const name = error?.name;
+  const message = error?.message || '-';
+  const eventType = event.type;
+  return name
+    ? `"${name}: ${message}" for event "${eventType}"`
+    : `Unknown error "${JSON.stringify(error)}" for event "${eventType}"`
+}
+
+const getOutgoingMessageForEvent = ({ type, metadata }: Event): string => {
+  const replyTo = metadata?.replyTo;
+  const correlationId = metadata?.correlationId;
+  return replyTo && correlationId
+    ? `${type}, id: ${correlationId} and sent to "${replyTo}"`
+    : correlationId
+      ? `${type}, id: ${correlationId}`
+      : type;
+}
+
+const getIncomingMessageForEvent = ({ type, metadata }: Event): string => {
+  const id = metadata?.correlationId;
+  return id ? `${type}, id: ${id}` : type;
+};
+
+const getErrorMessageForEvent = (event: Event<unknown>): string =>
+  isEventError(event.error)
+    ? getValidationErrorMessage(event)
+    : getGenericErrorMessage(event);
