ad41e767d9c030f249c7549da48cb4d61fa729f5
Jozef Flakus <monodies@me.com>
2020-08-30 00:01:00 +0200

    build: typescript v4.0 support

--- packages/middleware-io/src/io.middleware.ts
+++ packages/middleware-io/src/io.middleware.ts
@@ -1,8 +1,10 @@
 import * as t from 'io-ts';
 import { Reporter } from 'io-ts/lib/Reporter';
+import { identity } from 'fp-ts/lib/function';
 import * as E from 'fp-ts/lib/Either';
 import { Observable } from 'rxjs';
 import { map} from 'rxjs/operators';
+import { throwException } from '@marblejs/core/dist/+internal/utils';
 import { defaultReporter } from './io.reporter';
 import { IOError } from './io.error';
 
@@ -13,18 +15,19 @@ export interface ValidatorOptions {
   context?: string;
 }
 
-const validateError =
-  (reporter: Reporter<any> = defaultReporter, context?: string) =>
-  (result: E.Either<t.Errors, any>) => E.fold(
-    () => { throw new IOError('Validation error', reporter.report(result), context); },
-    res => res,
+const validateError = (reporter: Reporter<any> = defaultReporter, context?: string) => (result: E.Either<t.Errors, any>) =>
+  E.fold(
+    () => throwException(new IOError('Validation error', reporter.report(result), context)),
+    identity,
   )(result)
 
-export const validator$ = <U extends Schema, T>
-  (schema: U | undefined, options: ValidatorOptions = {}) => (i$: Observable<T>) =>
-    !!schema ? i$.pipe(
+export function validator$<U extends Schema, T>(schema: U, options?: ValidatorOptions): (i$: Observable<T>) => Observable<t.TypeOf<U>>;
+export function validator$<U extends Schema, T>(schema: undefined, options?: ValidatorOptions): (i$: Observable<T>) => Observable<T>;
+export function validator$<U extends Schema, T>(schema: U | undefined, options: ValidatorOptions = {}) {
+  return (i$: Observable<T>) =>
+    !schema ? i$ : i$.pipe(
       map(input => schema.decode(input)),
       map(validateError(options.reporter, options.context)),
-      map(input => input as t.TypeOf<typeof schema>),
-    ) : i$;
+    );
+}
 
