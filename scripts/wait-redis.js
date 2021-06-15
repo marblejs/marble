3079a9cced922de38ddaecccd03486ce3a53c7fd
JozefFlakus <monodies@me.com>
2021-06-15 09:40:09 +0200

    feat: support for TypeScript v4.3

--- scripts/wait-redis.js
+++ scripts/wait-redis.js
@@ -9,7 +9,7 @@ const log = msg => console.info(chalk.yellow(msg));
 const wait = async () => {
   log(' -- Waiting for Redis to be ready...');
   const client = await redis.createClient();
-  await new Promise((res, rej) => client.on('connect', err => err ? rej() : res()));
+  await new Promise((res, rej) => client.on('connect', err => err ? rej() : res(undefined)));
 }
 
 (async function() {
