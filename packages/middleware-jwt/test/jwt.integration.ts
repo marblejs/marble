da6f7e423db0555c0f0b9d312e972489434d8381
JozefFlakus <monodies@me.com>
2021-06-15 14:59:17 +0200

    build: rxjs upgrade - replaced deprecated  `flatMap` with `mergeMap`

--- packages/middleware-jwt/test/jwt.integration.ts
+++ packages/middleware-jwt/test/jwt.integration.ts
@@ -1,6 +1,6 @@
-import { httpListener, EffectFactory, combineRoutes } from '@marblejs/core';
+import { httpListener, r, combineRoutes } from '@marblejs/core';
 import { bodyParser$ } from '@marblejs/middleware-body';
-import { map, flatMap } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
 import { of, iif, throwError } from 'rxjs';
 import { authorize$ as authorizeMiddleware$, generateToken } from '../src';
 
@@ -11,7 +11,7 @@ export const SECRET_KEY = 'SOME_SSH_KEY';
 
 const verifyPayload$ = (payload: Payload) =>
   of(payload).pipe(
-    flatMap(payload => iif(
+    mergeMap(payload => iif(
       () => payload.id !== 'test_id' || payload.email !== 'admin@admin.com',
       throwError(() => new Error()),
       of(payload)
@@ -22,22 +22,22 @@ const authOptions = { secret: SECRET_KEY };
 const authorize$ = authorizeMiddleware$(authOptions, verifyPayload$);
 const generateFakeJWTPayload = ({ email }: LoginCredentials): Payload => ({ id: 'test_id', email });
 
-const login$ = EffectFactory
-  .matchPath('/login')
-  .matchType('POST')
-  .use(req$ => req$.pipe(
+const login$ = r.pipe(
+  r.matchPath('/login'),
+  r.matchType('POST'),
+  r.useEffect(req$ => req$.pipe(
     map(req => req.body as LoginCredentials),
     map(generateFakeJWTPayload),
     map(generateToken({ secret: SECRET_KEY })),
     map(token => ({ body: { token } })),
-  ));
+  )));
 
-const securedRoot$ = EffectFactory
-  .matchPath('/')
-  .matchType('GET')
-  .use(req$ => req$.pipe(
+const securedRoot$ = r.pipe(
+  r.matchPath('/'),
+  r.matchType('GET'),
+  r.useEffect(req$ => req$.pipe(
     map(req => ({ body: req.user })),
-  ));
+  )));
 
 const secured$ = combineRoutes('/secured', {
   effects: [securedRoot$],
