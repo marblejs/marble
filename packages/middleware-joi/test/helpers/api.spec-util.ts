4923f2bd4c0aff415c469071c7fb767cca0e2da8
JozefFlakus <monodies@me.com>
2021-06-16 10:51:16 +0200

    build: show and correct eslint deprecation warnings
    - show eslint deprecation warnings
    - corrected + suppresed selected `EffectFactory` deprecation warns
    - corrected `util` package deprecation warnings
    - middleware-joi: suppresd selected deprecation warnings
    - core: removed deprecated `createHttpServerTestBed`utility

--- packages/middleware-joi/test/helpers/api.spec-util.ts
+++ packages/middleware-joi/test/helpers/api.spec-util.ts
@@ -1,60 +1,58 @@
-import { httpListener, use, EffectFactory, combineRoutes } from '@marblejs/core';
+/* eslint-disable deprecation/deprecation */
+
+import { httpListener, use, r, combineRoutes } from '@marblejs/core';
 import { bodyParser$ } from '@marblejs/middleware-body';
 import { map } from 'rxjs/operators';
 import { validator$, Joi } from '../../src';
 
-const getPost$ = EffectFactory
-  .matchPath('/post')
-  .matchType('GET')
-  .use(req$ => req$
-    .pipe(
-      use(validator$({
-        query: Joi.object({ page: Joi.number().integer().greater(1) })
-      })),
-      map(req => req.query),
-      map(query => ({ status: 200, body: { ...query } }))
-    ));
+const getPost$ = r.pipe(
+  r.matchPath('/post'),
+  r.matchType('GET'),
+  r.useEffect(req$ => req$.pipe(
+    use(validator$({
+      query: Joi.object({ page: Joi.number().integer().greater(1) })
+    })),
+    map(req => req.query),
+    map(query => ({ status: 200, body: { ...query } }))
+  )));
 
-const getUser$ = EffectFactory
-  .matchPath('/user/:id')
-  .matchType('GET')
-  .use(req$ => req$
-    .pipe(
-      use(validator$({
-        params: {
-          id: Joi.number().integer().min(1).max(10).required(),
-        },
-      })),
-      map(req => req.params),
-      map(params => ({ status: 200, body: { id: params.id } }))
-    ));
+const getUser$ = r.pipe(
+  r.matchPath('/user/:id'),
+  r.matchType('GET'),
+  r.useEffect(req$ => req$.pipe(
+    use(validator$({
+      params: {
+        id: Joi.number().integer().min(1).max(10).required(),
+      },
+    })),
+    map(req => req.params),
+    map(params => ({ status: 200, body: { id: params.id } }))
+  )));
 
-const storePost$ = EffectFactory
-  .matchPath('/post')
-  .matchType('POST')
-  .use(req$ => req$
-    .pipe(
-      use(validator$({
-        query: Joi.object({ timestamp: Joi.date().required() }),
-        body: Joi.object({ title: Joi.string().required() }),
-      })),
-      map(resp => ({ body: { ...resp.body, ...resp.query } }))
-    ));
+const storePost$ = r.pipe(
+  r.matchPath('/post'),
+  r.matchType('POST'),
+  r.useEffect(req$ => req$.pipe(
+    use(validator$({
+      query: Joi.object({ timestamp: Joi.date().required() }),
+      body: Joi.object({ title: Joi.string().required() }),
+    })),
+    map(resp => ({ body: { ...resp.body, ...resp.query } }))
+  )));
 
-const postUser$ = EffectFactory
-  .matchPath('/user')
-  .matchType('POST')
-  .use(req$ => req$
-    .pipe(
-      use(validator$({
-        body: Joi.object({
-          name: Joi.string().required(),
-          passport: Joi.string().default('marble.js')
-        })
-      })),
-      map(req => req.body),
-      map(response => ({ body: response }))
-    ));
+const postUser$ = r.pipe(
+  r.matchPath('/user'),
+  r.matchType('POST'),
+  r.useEffect(req$ => req$.pipe(
+    use(validator$({
+      body: Joi.object({
+        name: Joi.string().required(),
+        passport: Joi.string().default('marble.js')
+      })
+    })),
+    map(req => req.body),
+    map(response => ({ body: response }))
+  )));
 
 const api$ = combineRoutes('/api', [getPost$, getUser$, postUser$, storePost$]);
 
