f7df9f90e62abb2045ce0434b126ccd64e8045cc
JozefFlakus <monodies@me.com>
2021-08-11 10:21:08 +0200

    refactor(http): content-related headers

--- packages/http/src/+internal/contentType.util.spec.ts
+++ packages/http/src/+internal/contentType.util.spec.ts
@@ -1,5 +1,7 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as O from 'fp-ts/lib/Option';
-import { getContentType, getContentTypeEncoding, getContentTypeUnsafe } from './contentType.util';
+import { ContentType, getContentType, getContentTypeEncoding, getContentTypeUnsafe, getMimeType } from './contentType.util';
 
 describe('#getContentType', () => {
   test('returns "some" if headers object contains "Content-Type" header', () => {
@@ -28,6 +30,14 @@ describe('#getContentTypeEncoding', () => {
     expect(getContentTypeEncoding({ 'content-type': 'application/json; charset=utf8' })).toEqual(O.some('utf8'));
   });
 
+  test('returns "some" if provided "Content-Type" value directly with "charset" part', () => {
+    expect(getContentTypeEncoding('application/json; charset=utf8')).toEqual(O.some('utf8'));
+  });
+
+  test('returns "some" with lowercased "charset" part', () => {
+    expect(getContentTypeEncoding('application/json; charset=UTF8')).toEqual(O.some('utf8'));
+  });
+
   test('returns "none" if headers object doesn\'t contain "Content-Type" header', () => {
     expect(getContentTypeEncoding({})).toEqual(O.none);
   });
@@ -37,5 +47,43 @@ describe('#getContentTypeEncoding', () => {
     expect(getContentTypeEncoding({ 'content-type': 'application/json;' })).toEqual(O.none);
     expect(getContentTypeEncoding({ 'Content-Type': 'application/json; charset' })).toEqual(O.none);
     expect(getContentTypeEncoding({ 'Content-Type': 'application/json; charset=' })).toEqual(O.none);
+  });
+});
+
+describe('#getMimeType', () => {
+  test('detects mime-type from path', () => {
+    // given
+    const body = Buffer.from('test');
+    const pathToIndex = '/test/index.html';
+
+    // when
+    const mimeType = getMimeType(body, pathToIndex);
+
+    // then
+    expect(mimeType).toEqual(ContentType.TEXT_HTML);
+  });
+
+  test('detects mime-type from buffer', () => {
+    // given
+    const body = fs.readFileSync(path.resolve(__dirname, '../../../../assets/img', 'logo.png'));
+    const pathToIndex = '/test/index.html';
+
+    // when
+    const mimeType = getMimeType(body, pathToIndex);
+
+    // then
+    expect(mimeType).toEqual(ContentType.IMAGE_PNG);
+  });
+
+  test('returns null if cannot guess mime-type ', () => {
+    // given
+    const body = Buffer.from('test');
+    const pathToIndex = '/test/index';
+
+    // when
+    const mimeType = getMimeType(body, pathToIndex);
+
+    // then
+    expect(mimeType).toEqual(null);
   });
 });
