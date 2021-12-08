4e491ac4df835a9026fd65122449eab0adaadc46
JozefFlakus <monodies@me.com>
2021-12-08 09:03:20 +0100

    fix(http): collect request metadata in #subscribeResponse

--- packages/http/src/server/internal-dependencies/httpRequestMetadataStorage.reader.spec.ts
+++ packages/http/src/server/internal-dependencies/httpRequestMetadataStorage.reader.spec.ts
@@ -1,24 +1,44 @@
 import { createContext } from '@marblejs/core';
 import { createUuid } from '@marblejs/core/dist/+internal/utils';
-import { HttpRequestMetadata } from '../../http.interface';
+import { HttpEffectResponse, HttpRequest } from '../..';
+import { HTTP_REQUEST_METADATA_ID_HEADER_KEY } from '../../+internal/metadata.util';
+import { HttpRequestMetadata, WithHttpRequest } from '../../http.interface';
 import { HttpRequestMetadataStorage } from './httpRequestMetadataStorage.reader';
 
 describe('#HttpRequestMetadataStorage', () => {
-  const storage = HttpRequestMetadataStorage(createContext());
-
   test('sets => gets => removes value from storage', () => {
+    // given
     const id = createUuid();
+    const storage = HttpRequestMetadataStorage(createContext());
     const metadata: HttpRequestMetadata = { test: 'test_data' };
 
     expect(storage.size()).toEqual(0);
 
+    // when
     storage.set(id, metadata);
 
+    // then
     expect(storage.size()).toEqual(1);
+    expect(storage.get(id)).toEqual(metadata);
+    expect(storage.size()).toEqual(0);
+  });
 
-    const retrievedData = storage.get(id);
+  test('collects metadata from request', () => {
+    // given
+    const storage = HttpRequestMetadataStorage(createContext());
+    const meta: HttpRequestMetadata = { test: 'test_data' };
+    const requestId = createUuid();
+    const request = { meta, headers: { [HTTP_REQUEST_METADATA_ID_HEADER_KEY]: requestId } } as any as  HttpRequest;
+    const response = { request } as WithHttpRequest<HttpEffectResponse>;
 
-    expect(retrievedData).toEqual(metadata);
+    expect(storage.size()).toEqual(0);
+
+    // when
+    storage.collect(response);
+
+    // then
+    expect(storage.size()).toEqual(1);
+    expect(storage.get(requestId)).toEqual(meta);
     expect(storage.size()).toEqual(0);
   });
 });
