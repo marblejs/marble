b6462363f1b29fc465836b25c048ce092ef04aec
Józef Flakus <monodies@me.com>
2022-12-01 11:02:33 +0100

    feat(messaging): redis - an "UnsupportedError" is not thrown when calling "ackMessage/nackMessage"

--- packages/messaging/src/ack/ack.ts
+++ packages/messaging/src/ack/ack.ts
@@ -1,7 +1,7 @@
 import { promisify } from 'util';
 import * as T from 'fp-ts/lib/Task';
-import { pipe } from 'fp-ts/lib/pipeable';
-import { constTrue } from 'fp-ts/lib/function';
+import * as E from 'fp-ts/lib/Either';
+import { pipe, constTrue } from 'fp-ts/lib/function';
 import { Event, EffectContext, useContext } from '@marblejs/core';
 import { TransportLayerConnection } from '../transport/transport.interface';
 import { EventTimerStore, EventTimerStoreToken } from '../eventStore/eventTimerStore';
@@ -14,9 +14,17 @@ const timeoutTask = () => promisify(setTimeout)(10);
 
 const unregisterTask = (store: EventTimerStore) => (e: Event) => T.fromIO(store.unregister(e));
 
-const ackTask = (ctx: EffectContext<TransportLayerConnection>) => (e: Event) => T.fromIO(() => ctx.client.ackMessage(e.metadata?.raw));
+const ackTask = (ctx: EffectContext<TransportLayerConnection>) => (e: Event): T.Task<void> =>
+  pipe(
+    E.tryCatch(() => ctx.client.ackMessage(e.metadata?.raw), E.toError),
+    E.fold(() => T.of(undefined), () => T.of(undefined)),
+  );
 
-const nackTask = (ctx: EffectContext<TransportLayerConnection>) => (e: Event) => (resend: boolean) => T.fromIO(() => ctx.client.nackMessage(e.metadata?.raw, resend));
+const nackTask = (ctx: EffectContext<TransportLayerConnection>) => (e: Event) => (resend: boolean): T.Task<void> =>
+  pipe(
+    E.tryCatch(() => ctx.client.nackMessage(e.metadata?.raw, resend), E.toError),
+    E.fold(() => T.of(undefined), () => T.of(undefined)),
+  );
 
 export const ackEvent = (ctx: EffectContext<TransportLayerConnection>) =>
   pipe(
